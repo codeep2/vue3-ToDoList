6e5343399315365996cd3109d2ba385d503391f1
codeep2 <codeep37@gmail.com>
2020-10-28 17:53:41 +0800

    feat: 完成通用header基本样式

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+div.header-wrap
+  h1 {{ $route.name }}
+  slot
+</template>
+
+<style lang="scss" scoped>
+  .header-wrap {
+    display: flex;
+    font-size: 20px;
+    padding: 0 0 10px 10px;
+    text-transform: capitalize;
+    border-bottom: solid 3px #878787;
+  }
+</style>
