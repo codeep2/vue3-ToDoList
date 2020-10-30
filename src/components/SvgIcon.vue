42e7ac3644f06dd312509a779f85ad3eb9ab67f3
codeep2 <codeep37@gmail.com>
2020-10-30 17:47:25 +0800

    feat: 将svg资源导入封装成component

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+svg.svg_icon
+  use(:xlink:href="`#${iconName}`")
+</template>
+
+<script>
+export default {
+  name: 'SvgIcon',
+  props: {
+    iconName: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
